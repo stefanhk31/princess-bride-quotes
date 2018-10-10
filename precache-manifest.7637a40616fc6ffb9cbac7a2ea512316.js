98865d5ef966df6aa1dc03ca3990ca8a216f9245
Stefan <shodgeskluck@gmail.com>
2018-10-10 10:01:30 -0400

    Updates

---
+++ precache-manifest.7637a40616fc6ffb9cbac7a2ea512316.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/princess-bride-quotes/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "db425ef448fa6623b752",
+    "url": "/princess-bride-quotes/static/js/runtime~main.db425ef4.js"
+  },
+  {
+    "revision": "0c1876441c9ce2c943cd",
+    "url": "/princess-bride-quotes/static/js/main.0c187644.chunk.js"
+  },
+  {
+    "revision": "ccf717e8f1c052517a1a",
+    "url": "/princess-bride-quotes/static/js/1.ccf717e8.chunk.js"
+  },
+  {
+    "revision": "0c1876441c9ce2c943cd",
+    "url": "/princess-bride-quotes/static/css/main.416c0ad9.chunk.css"
+  },
+  {
+    "revision": "9914c1c5962377d08aec1084a0ec4d66",
+    "url": "/princess-bride-quotes/index.html"
+  }
+];
